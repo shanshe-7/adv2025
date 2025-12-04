92b2ef2e4ebed70f1b3eb4392b6aa38ff4ec827a
shanshe-7 <shanshe1993@gmail.com>
2025-12-05 01:25:46 +0400

    feat: start 2

---
+++ day4/part2.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+
+const readline = require("readline");
+
+const combinations = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+  [1, 1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+];
+
+async function getRollsOfPaper() {
+  const fStream = fs.createReadStream("day4/input.txt");
+  const rl = readline.createInterface({
+    input: fStream,
+    crlfDelay: Infinity,
+  });
+
+  let initRolls = [];
+
+  for await (line of rl) {
+    initRolls.push(line?.trim());
+  }
+
+  calculateRolls(initRolls);
+}
+
+/**
+ * @param {string[]} rolls
+ */
+
+function calculateRolls(rolls = [], removeArr = null) {
+  if (typeof removeArr === "object") {
+    if (removeArr?.length === 0) return;
+  }
+  let tempArr = [];
+
+  let temp = 0;
+
+  for (let i = 0; i < rolls.length; i++) {
+    let element = rolls[i];
+
+    let counter = 0;
+
+    for (let j = 0; j < element.length; j++) {
+      if (element[j] === "@") {
+        let col = j;
+        let row = i;
+        let colEnd = element.length;
+        let rowEnd = rolls.length;
+
+        combinations.forEach((shift) => {
+          const [rowSh, colSh] = shift;
+
+          let searRow = row + rowSh;
+          let searCol = col + colSh;
+
+          if (
+            searRow >= 0 &&
+            searCol >= 0 &&
+            searRow < rowEnd &&
+            searCol < colEnd
+          ) {
+            if (rolls[searRow][searCol] === "@") {
+              counter += 1;
+            }
+          }
+        });
+
+        if (counter < 4) {
+          temp += 1;
+        }
+        counter = 0;
+      }
+    }
+  }
+  console.log(tempArr, "att");
+
+  console.log(temp, "t");
+}
+
+getRollsOfPaper();
